ed24583985ea347b4874cbcd2c225440a1d7af3c
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-03-13 00:38:30 -0500

    chore: add service to import data

--- db/migrate/20250312180023_add_information_to_users.rb
+++ db/migrate/20250312180023_add_information_to_users.rb
@@ -2,5 +2,6 @@ class AddInformationToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :role, :integer, default: 0, null: false
     add_reference :users, :leader, foreign_key: { to_table: :users }, null: true
+    add_column :users, :position, :integer, default: 0, null: false
   end
 end
